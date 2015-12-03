91466e53660e7cae9111e21140d7c3f40731d2c5
Jianfeng Chen <jchen37@login02.hpc.ncsu.edu>
2015-12-02 23:33:39 -0500

    init commint

---
+++ jmoo_hpc_load.py
@@ -0,0 +1,14 @@
+import pickle
+import sys
+
+with open('hpc_dumps/tmp_jmoo_core_obj','rb') as f:
+    [core,tag] = pickle.load(f)
+
+index = int(sys.argv[1])
+if index > len(core.tests.algorithms):
+    print 'no!no!no! index error at hpc job submitting'
+    exit(0)
+
+core.tests.algorithms = core.tests.algorithms[index:index+1]
+core.doTests()
+core.doReports(tag)
